75e49958691bb25c5a395806fc7f889f7adcd7ac
Sswmhskr06ksj <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-04-12 17:48:56 +0900

    Calculator easy
    더하기, 빼기, 곱하기, 나누기만 있는 쉬운 계산기를 만들어 보았습니다.
    2022/4/12

---
+++ Tutorial2/app/src/main/java/com/hs/tutorial2/MainActivity.java
@@ -0,0 +1,53 @@
+package com.hs.tutorial2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result =  (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1+n2));
+    }
+
+    public void subtractClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result =  (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1-n2));
+    }
+
+    public void multiplylick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result =  (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1*n2));
+    }
+
+    public void divideClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result =  (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1/n2));
+    }
+}
